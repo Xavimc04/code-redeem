dc7947c27e0e0aa69d7ba4f88f7f7c50411c4e18
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-05-26 18:21:40 +0200

    Refactor code redeem command for better validation and error handling

--- server/main.lua
+++ server/main.lua
@@ -1,31 +1,18 @@
 ESX = exports['es_extended']:getSharedObject()
 
-<<<<<<< HEAD
 RegisterCommand(Config.Command, function(source, args) 
-=======
-RegisterCommand('canjearcodigo', function(source, args) 
->>>>>>> 43eaaa6d120161417749e15b58b6d534fb2b116b
     local src = source 
 
     if not src then
         return 
     end
 
-<<<<<<< HEAD
-=======
-    if not args[1] then
-        ESX.ShowNotification(src, "Debes introducir un código.")
-    end
-
-    local code = args[1]
->>>>>>> 43eaaa6d120161417749e15b58b6d534fb2b116b
     local Player = ESX.GetPlayerFromId(src)
 
     if not Player then
         return 
     end
 
-<<<<<<< HEAD
     if not args[1] then
         Player.showNotification(Config.Translations['no_code'])
     end
@@ -36,33 +23,20 @@ RegisterCommand('canjearcodigo', function(source, args)
 
     if not discord then
         Player.showNotification(Config.Translations['no_discord'])
-=======
-    local discord = getPlayerDiscord(src)
-
-    if not discord then
-        ESX.ShowNotification(src, "Debes tener tu Discord vinculado para canjear códigos.")
->>>>>>> 43eaaa6d120161417749e15b58b6d534fb2b116b
 
         return 
     end
 
-<<<<<<< HEAD
     -- @ Validate code is unclaimed
     PerformHttpRequest(Config.Website.URL .. '?code=' .. code, function (errorCode, resultData, resultHeaders, errorData)
         if errorCode ~= 200 then
             Player.showNotification(Config.Translations['invalid_code'])
-=======
-    PerformHttpRequest(WEBSITE_URL, function (errorCode, resultData, resultHeaders, errorData)
-        if errorCode ~= 200 then
-            ESX.ShowNotification(src, "El código no es válido o ya ha sido canjeado.")
->>>>>>> 43eaaa6d120161417749e15b58b6d534fb2b116b
 
             return 
         end
 
         local data = json.decode(resultData)
 
-<<<<<<< HEAD
         if data.code.status == 'claimed' then
             Player.showNotification(Config.Translations['invalid_code'])
 
@@ -113,13 +87,4 @@ RegisterCommand('canjearcodigo', function(source, args)
             })
         end
     end)
-=======
-        print(resultData)
-    end, 'POST', json.encode({
-        code = code,
-        secret = WEBSITE_SECRET
-    }), {
-        ['Content-Type'] = 'application/json'
-    })
->>>>>>> 43eaaa6d120161417749e15b58b6d534fb2b116b
 end)
